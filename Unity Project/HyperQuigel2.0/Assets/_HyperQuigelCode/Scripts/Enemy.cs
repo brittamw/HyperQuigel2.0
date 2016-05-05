0c835c844037a276f1c2dc7448f676224633c547
pete7 <matthias.miro@gmail.com>
2016-05-05 21:46:35 +0200

    es kommen zwei verschiedene Enemys auf einen zu und man kann sie mit verschiedenen Aktionen (Maustasten) töten

---
+++ Unity Project/HyperQuigel2.0/Assets/_HyperQuigelCode/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    Transform player;
+
+	bool alive;
+
+	Rigidbody enemey;
+    NavMeshAgent nav;
+
+
+    void Start () {
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+		nav = GetComponent<NavMeshAgent>();
+		alive = true;
+		enemey = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (alive) {
+			nav.SetDestination (player.position);
+		}
+	}
+
+	public void Die() {
+		nav.enabled = false;
+		Vector3 force = new Vector3 (-5, 40, 50);
+		enemey.AddForce (force, ForceMode.Impulse);
+		alive = false;
+	}
+}
---
+++ Unity Project/HyperQuigel2.0/Assets/_HyperQuigelCode/Scripts/EnemyManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyManager : MonoBehaviour {
+
+	public GoodEnemy goodEnemy;
+	public BadEnemy badEnemy;
+
+	ArrayList enemyList;
+
+	int currentEnemy;
+	int timeBetweenEnemyKilling;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("createEnemy", 0f, 2f);
+		enemyList = new ArrayList ();
+		currentEnemy = -1;
+		timeBetweenEnemyKilling = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (timeBetweenEnemyKilling == 0) {
+			bool killGoodEnemy = false;
+			bool killBadEnemy = false;
+			if (Input.GetButtonDown ("Fire1")) {
+				killGoodEnemy = true;
+			} else if (Input.GetButtonDown("Fire2")) {
+				killBadEnemy = true;
+			}
+			if (killBadEnemy || killGoodEnemy) {
+				if (currentEnemy < enemyList.Count) {
+					Enemy enemy = (Enemy) enemyList [currentEnemy];
+					if (enemy is GoodEnemy && killGoodEnemy) {
+						KillEnemy (enemy);
+					} else if (enemy is BadEnemy && killBadEnemy) {
+						KillEnemy (enemy);
+					}
+				}
+			}		
+		}
+
+		if (timeBetweenEnemyKilling > 0) {
+			timeBetweenEnemyKilling--;
+		}
+	}
+
+	void KillEnemy(Enemy enemy) {
+		enemy.Die ();
+		currentEnemy++;
+		timeBetweenEnemyKilling = 5;
+	}
+
+	void createEnemy() {
+		float randomForPos = Random.value-0.5f;
+		Vector3 randomPos = new Vector3(randomForPos*50,0,0);
+
+		float randomForType = Random.value*2f;
+		Enemy newEnemyObject;
+		if (Mathf.Floor(randomForType) == 0) {
+			newEnemyObject = (Enemy) Instantiate (goodEnemy, randomPos, goodEnemy.transform.rotation);
+		} else {
+			newEnemyObject = (Enemy) Instantiate (badEnemy, randomPos, badEnemy.transform.rotation);
+		}
+
+		enemyList.Add (newEnemyObject);
+		// nur beim ersten Enemy
+		if (currentEnemy == -1) {
+			currentEnemy = 0;
+		}
+	}
+}
